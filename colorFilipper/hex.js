2bc082f24d83a3a65396f327c950fcaa696d484c
wocjechdominiak <wojciech.dominiak96@gmail.com>
2022-03-30 19:06:10 +0200

    counter 3 and collorflipper

---
+++ colorFilipper/hex.js
@@ -0,0 +1,21 @@
+//set array with necessary value for drawing a color
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+//select span and button
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+//logic
+btn.addEventListener("click", function () {
+  let hexColor = "#";
+  for (let i = 0; i < 6; i++) {
+    hexColor += hex[getRandomNumber()];
+  }
+  color.textContent = hexColor;
+  document.body.style.backgroundColor = hexColor;
+});
+
+//get random number between 0-15
+function getRandomNumber() {
+  return Math.floor(Math.random() * hex.length);
+}
---
+++ colorFilipper/simple.js
@@ -0,0 +1,18 @@
+//set array with sample colors
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+//select span and button
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+//logic
+btn.addEventListener("click", function () {
+  const randomNumber = getRandomNumber();
+  document.body.style.backgroundColor = colors[randomNumber];
+  color.textContent = colors[randomNumber];
+});
+
+//get random number between 0-3
+function getRandomNumber() {
+  return Math.floor(Math.random() * colors.length);
+}
